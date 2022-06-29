52cd9c1514800ab9a15a8c04ae302d12609454ec
THOMAS HARDING <hardingt604@gmail.com>
2022-06-29 11:28:57 -0600

    add cat /get all test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,16 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+// const { agent } = require('supertest');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('GET /cats gets a list of all cats', async() => {
+    const resp = await request(app).get('/cats');
+    expect(resp.status).toEqual(200);
+    // expect(resp.body).toEqual(the entire cats array i dont want to write it)
   });
   afterAll(() => {
     pool.end();
