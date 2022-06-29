048fb17b3b9e5422436d94149aa6fdda658a6e99
THOMAS HARDING <hardingt604@gmail.com>
2022-06-29 11:36:51 -0600

    add actual data to the expected result in cat tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,7 +11,13 @@ describe('backend-express-template routes', () => {
   it('GET /cats gets a list of all cats', async() => {
     const resp = await request(app).get('/cats');
     expect(resp.status).toEqual(200);
-    // expect(resp.body).toEqual(the entire cats array i dont want to write it)
+    expect(resp.body).toEqual([
+      { 'id':'1', 'name':'Soup', 'age':3, 'eyes':'blue', 'fur':'tan' },
+      { 'id':'2', 'name':'Cricket', 'age':16, 'eyes':'black', 'fur':'brown' },
+      { 'id':'3', 'name':'Bear', 'age':8, 'eyes':'brown', 'fur':'black' },
+      { 'id':'4', 'name':'Pasta', 'age':1, 'eyes':'yellow', 'fur':'gold' },
+      { 'id':'5', 'name':'Howard', 'age':9, 'eyes':'red', 'fur':'brown' },
+      { 'id':'6', 'name':'Otter', 'age':0, 'eyes':'blue', 'fur':'white' }]);
   });
   afterAll(() => {
     pool.end();
